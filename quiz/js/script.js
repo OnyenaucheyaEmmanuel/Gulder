132d21483936f817bc904c54840041cb09520710
Onyenaucheya Emmanuel <94042276+OnyenaucheyaEmmanuel@users.noreply.github.com>
2023-10-27 08:37:49 +0100

    Update script.js

--- quiz/js/script.js
+++ quiz/js/script.js
@@ -41,24 +41,24 @@ let widthValue = 0;
 const restart_quiz = result_box.querySelector(".buttons .restart");
 const quit_quiz = result_box.querySelector(".buttons .quit");
 
-// if restartQuiz button clicked
-restart_quiz.onclick = () => {
-  quiz_box.classList.add("activeQuiz"); //show quiz box
-  result_box.classList.remove("activeResult"); //hide result box
-  timeValue = 60;
-  que_count = 0;
-  que_numb = 1;
-  userScore = 0;
-  widthValue = 0;
-  showQuetions(que_count); //calling showQestions function
-  queCounter(que_numb); //passing que_numb value to queCounter
-  clearInterval(counter); //clear counter
-  clearInterval(counterLine); //clear counterLine
-  startTimer(timeValue); //calling startTimer function
-  startTimerLine(widthValue); //calling startTimerLine function
-  timeText.textContent = "Timer Counting"; //change the text of timeText to Time Left
-  next_btn.classList.remove("show"); //hide the next button
-};
+// // if restartQuiz button clicked
+// restart_quiz.onclick = () => {
+//   quiz_box.classList.add("activeQuiz"); //show quiz box
+//   result_box.classList.remove("activeResult"); //hide result box
+//   timeValue = 60;
+//   que_count = 0;
+//   que_numb = 1;
+//   userScore = 0;
+//   widthValue = 0;
+//   showQuetions(que_count); //calling showQestions function
+//   queCounter(que_numb); //passing que_numb value to queCounter
+//   clearInterval(counter); //clear counter
+//   clearInterval(counterLine); //clear counterLine
+//   startTimer(timeValue); //calling startTimer function
+//   startTimerLine(widthValue); //calling startTimerLine function
+//   timeText.textContent = "Timer Counting"; //change the text of timeText to Time Left
+//   next_btn.classList.remove("show"); //hide the next button
+// };
 
 // if quitQuiz button clicked
 // quit_quiz.onclick = () => {
